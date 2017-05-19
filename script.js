c3231752f2859a0b45899c7963df8f450570a42d
Stef Kors <stef.kors@gmail.com>
2017-05-19 21:15:12 +0200

    hover img changes

--- script.js
+++ script.js
@@ -7,4 +7,29 @@ $(document).ready(function() {
 		$('.arrow').toggleClass('img_flip');
 	});
 
+	var joost = 'Joost Kors, aged 27, is a designer with an interest in concrete buildings, landscape art and unconventional spacitial planning. With a background in ceramics as building material Joost explores heritage and innovation. Now as a recent graduate, he is available for work.';
+
+	var binck = '';
+
+	var binck = '';
+
+	var aho = '';
+
+	var hybrid = '';
+
+	var nai = '';
+
+	var expo = '';
+
+	var dsm = '';
+
+	$('h1').mouseover(function() {
+		$('.project_img').attr("src", 'img/' + this.id + '.jpg');
+
+		$('.paragraph').html(eval(this.id));
+	}).mouseleave(function() {
+		$('.project_img').attr("src", '');
+
+		$('.paragraph').html('');
+	});
 });
