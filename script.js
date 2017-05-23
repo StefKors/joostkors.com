5f96dee6f425074c4caa9f58a821d059e3cffddf
Stef Kors <stef.kors@gmail.com>
2017-05-23 18:15:16 +0200

    fade in out script

--- script.js
+++ script.js
@@ -9,27 +9,33 @@ $(document).ready(function() {
 
 	var joost = 'Joost Kors, aged 27, is a designer with an interest in concrete buildings, landscape art and unconventional spacitial planning. With a background in ceramics as building material Joost explores heritage and innovation. Now as a recent graduate, he is available for work.';
 
-	var binck = '';
+	var binck = 'The Fake News Data Sprint is pleased to have Jayson Harsin (The ... teaching, design and participation formats for the creation and use of';
 
-	var binck = '';
+	var aho = 'Dance is a performing art form consisting of purposefully selected sequences of human movement. This movement has aesthetic and symbolic value, and is acknowledged as dance by performers and observers within a particular culture. ';
 
-	var aho = '';
+	var hybrid = 'The potato is a starchy, tuberous crop from the perennial nightshade Solanum tuberosum. The word "potato" may refer either to the plant itself or to the edible tuber.';
 
-	var hybrid = '';
+	var nai = 'Dance Dance Revolution, abbreviated DDR and also known as Dancing Stage in earlier games in Europe and Australasia, and some other games in Japan, is a music video game series produced by Konami.';
 
-	var nai = '';
+	var expo = 'Maize, also known as corn, is a large grain plant first domesticated by indigenous peoples in southern Mexico about 10,000 years ago.';
 
-	var expo = '';
-
-	var dsm = '';
+	var dsm = 'Mar hoe zit het noow mae de twedde vörm die zö duk vurkumt ien het Rivierengebied? Volgens t Rheinisches Wörterbuch kumt pieper van de ';
 
 	$('h1').mouseover(function() {
-		$('.project_img').attr("src", 'img/' + this.id + '.jpg');
+		var this_one = this.id;
+		// check if url is same as current url
+		if ($('.project_img').attr("src") != 'img/' + this_one + '.jpg') {
+			// if not same change img
+			// fadeout current image
+			$('.project_img').fadeOut(200, function() {
+				// fade in new image
+				$('.project_img').attr("src", 'img/' + this_one + '.jpg').fadeIn(200);
+			});
 
-		$('.paragraph').html(eval(this.id));
-	}).mouseleave(function() {
-		$('.project_img').attr("src", '');
-
-		$('.paragraph').html('');
+			// do same thing with the text
+			$('.paragraph').fadeOut(200, function() {
+				$('.paragraph').html(eval(this_one)).fadeIn(200);
+			});
+		}
 	});
 });
